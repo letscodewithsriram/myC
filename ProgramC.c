715744ec964f9e7e2d064400533af1dfd71bda23
letscodewithsriram <sriram.researcher@gmail.com>
2025-04-19 15:48:00 -0700

    Project Euler 02

--- ProgramC.c
+++ ProgramC.c
@@ -1,11 +1,9 @@
 # include <stdio.h>
 
-
 int myFirstFunction(void) {
     printf("Hello, World!");
     return 0;
 }
-
 
 int myPeuler01(int max) {
     int sum = 0;
@@ -19,11 +17,32 @@ int myPeuler01(int max) {
     return sum;
 }
 
+int myPeuler02(int nums) {
+    int num0, num1;
+    num0 = 0;
+    num1 = 1;
+    int fSeries;
+    int sum = 0;
+    for (int i=0; i<nums; i++) {
+            fSeries = num0 + num1;
+            num0 = num1;
+            num1 = fSeries;
+            if (fSeries < 4000000 && fSeries % 2 == 0) {
+                printf ("%d + %d = %d \n", num0, num1, fSeries);
+                sum = sum + fSeries;
+            }
+            if (fSeries > 4000000){
+                break;
+            }
+    }
+    return sum;
+}
 
 int main (void)
 {
-    printf("%d", myPeuler01(1000));
-    myFirstFunction();
+    printf("%d", myPeuler02(1000000));
+    // printf("%d", myPeuler01(1000));
+    // myFirstFunction();
     return 0;
 }
 
