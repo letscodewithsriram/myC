e0e27a42331bfdb082307763f2ba83b22275d9a5
letscodewithsriram <sriram.researcher@gmail.com>
2025-04-19 15:30:14 -0700

    Project Euler 01

--- ProgramC.c
+++ ProgramC.c
@@ -1,1 +1,31 @@
-#include <stdio.h>
+# include <stdio.h>
+
+
+int myFirstFunction(void) {
+    printf("Hello, World!");
+    return 0;
+}
+
+
+int myPeuler01(int max) {
+    int sum = 0;
+    for (int i=1; i < max; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            printf("%d\n", i);
+            sum = sum + i;
+        }
+    }
+    // printf("%d", sum);
+    return sum;
+}
+
+
+int main (void)
+{
+    printf("%d", myPeuler01(1000));
+    myFirstFunction();
+    return 0;
+}
+
+
+
